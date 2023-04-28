31c40ba501487c11ab125b17fd5445202be6a7e9
jessl33 <Leesyj98@gmail.com>
2023-04-28 14:20:20 +0800

    update

---
+++ GITtesting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GITtesting.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
